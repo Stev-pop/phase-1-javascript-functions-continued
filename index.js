aa6a537a5f919c792ca1dd04b3d43532cd4a1eaf
Stephen Onyango <onyangosteve01@gmail.com>
2024-03-21 01:04:07 +0300

    Adds functions

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+function saturdayFun(activity ='roller-skate' ){
+    return `This Saturday, I want to ${activity}!`;
+    }
+    saturdayFun("bathe my dog");
+    
+function mondayWork(action = 'go to the office'){
+    return `This Monday, I will ${action}.`;
+    }
+    mondayWork("work from home");
+
+function wrapAdjective(visualFlair) {
+        return function(adjective) {
+          return `You are ${visualFlair}${adjective}${visualFlair}!`;
+        };
+      }
+      wrapAdjective("%")("a dedicated programmer");
+
